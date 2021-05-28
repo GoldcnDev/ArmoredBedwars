9aa50ecf9148bceb633bc11f39d3a7362dc591f8
GoldcnDev <44988826+GoldcnDev@users.noreply.github.com>
2021-05-27 22:45:04 -0400

    much

--- src/me/dzze/gameframework/listeners/BedListener.java
+++ src/me/dzze/gameframework/listeners/BedListener.java
@@ -24,30 +24,6 @@ public class BedListener implements Listener {
     @EventHandler
     public void onEnable(BlockBreakEvent e){
         Player p = e.getPlayer();
-        if(e.getBlock().getLocation() == new Location(Bukkit.getWorld(main.getConfig().getString("Purple.world")), main.getConfig().getDouble("Purple.x"),
-                main.getConfig().getDouble("Purple.y"), main.getConfig().getDouble("Purple.z"))){
-            Bed purple = (Bed) e.getBlock();
-            Bukkit.getServer().broadcastMessage(MessageUtils.color("&3&lB&B&lW &8| &cThe &5&lPURPLE &cbed has been destroyed!"));
-            e.setDropItems(false);
-        }
-        if(e.getBlock().getLocation() == new Location(Bukkit.getWorld(main.getConfig().getString("Red.world")), main.getConfig().getDouble("Red.x"),
-                main.getConfig().getDouble("Red.y"), main.getConfig().getDouble("Red.z"))){
-            Bed red = (Bed) e.getBlock();
-            Bukkit.getServer().broadcastMessage(MessageUtils.color("&3&lB&B&lW &8| &cThe &c&lRED &cbed has been destroyed!"));
-            e.setDropItems(false);
-        }
-        if(e.getBlock().getLocation() == new Location(Bukkit.getWorld(main.getConfig().getString("White.world")), main.getConfig().getDouble("White.x"),
-                main.getConfig().getDouble("White.y"), main.getConfig().getDouble("White.z"))){
-            Bed white = (Bed) e.getBlock();
-            Bukkit.getServer().broadcastMessage(MessageUtils.color("&3&lB&B&lW &8| &cThe &f&lWHITE &cbed has been destroyed!"));
-            e.setDropItems(false);
-        }
-        if(e.getBlock().getLocation() == new Location(Bukkit.getWorld(main.getConfig().getString("Blue.world")), main.getConfig().getDouble("Blue.x"),
-                main.getConfig().getDouble("Blue.y"), main.getConfig().getDouble("Blue.z"))){
-            Bed blue = (Bed) e.getBlock();
-            Bukkit.getServer().broadcastMessage(MessageUtils.color("&3&lB&B&lW &8| &cThe &9&lBLUE &cbed has been destroyed!"));
-            e.setDropItems(false);
-        }
         if(SetBedBlue.settingBed.contains(p)){
             e.setCancelled(true);
             main.getConfig().set("Blue.world", p.getWorld().getName());
@@ -87,6 +63,26 @@ public class BedListener implements Listener {
             main.saveConfig();
             SetBedPurple.settingBed.remove(p);
             MessageUtils.message(p, "&3&lB&b&lW &8| &aSet the location of the Purple bed.");
+        }
+        if(e.getBlock().getLocation() == new Location(Bukkit.getWorld(main.getConfig().getString("Purple.world")), main.getConfig().getDouble("Purple.x"),
+                main.getConfig().getDouble("Purple.y"), main.getConfig().getDouble("Purple.z"))){
+            Bukkit.getServer().broadcastMessage(MessageUtils.color("&3&lB&B&lW &8| &cThe &5&lPURPLE &cbed has been destroyed!"));
+            e.setDropItems(false);
+        }
+        if(e.getBlock().getLocation() == new Location(Bukkit.getWorld(main.getConfig().getString("Red.world")), main.getConfig().getDouble("Red.x"),
+                main.getConfig().getDouble("Red.y"), main.getConfig().getDouble("Red.z"))){
+            Bukkit.getServer().broadcastMessage(MessageUtils.color("&3&lB&B&lW &8| &cThe &c&lRED &cbed has been destroyed!"));
+            e.setDropItems(false);
+        }
+        if(e.getBlock().getLocation() == new Location(Bukkit.getWorld(main.getConfig().getString("White.world")), main.getConfig().getDouble("White.x"),
+                main.getConfig().getDouble("White.y"), main.getConfig().getDouble("White.z"))){
+            Bukkit.getServer().broadcastMessage(MessageUtils.color("&3&lB&B&lW &8| &cThe &f&lWHITE &cbed has been destroyed!"));
+            e.setDropItems(false);
+        }
+        if(e.getBlock().getLocation() == new Location(Bukkit.getWorld(main.getConfig().getString("Blue.world")), main.getConfig().getDouble("Blue.x"),
+                main.getConfig().getDouble("Blue.y"), main.getConfig().getDouble("Blue.z"))){
+            Bukkit.getServer().broadcastMessage(MessageUtils.color("&3&lB&B&lW &8| &cThe &9&lBLUE &cbed has been destroyed!"));
+            e.setDropItems(false);
         }
     }
 
